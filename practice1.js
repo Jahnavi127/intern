9d46ab63d3b55613dfeb3ff313bcaaa0374cd6fa
Andhavarapu Jahnavi <janujasu127@gmail.com>
2023-12-14 15:57:45 +0530

    commiting changes

--- practice1.js
+++ practice1.js
@@ -75,5 +75,63 @@ for(i=0;i<arr.length;i++){
             val=arr[i];
         }
 }
-
 console.log(val,answer);
+//9
+let isUpperCase=(i)=> i===i.toUpperCase();
+let s = 'The Quick Brown Fox';
+ans="";
+for(i of s){
+    if(isUpperCase(i)){
+        ans+=i.toLowerCase();
+    }
+    else{
+        ans+=i.toUpperCase();
+    }
+}
+console.log(ans);
+//10
+a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+j=0;
+for(i of a){
+    console.log("row"+j)
+    for(let k of i){
+        console.log(k);
+    }
+    j++;
+}
+//11(Binary search)
+a= [1, 2, 3, 4, 5, 7, 8, 9];
+let binary_search=(a,i)=>{
+    let l=0,h=a.length-1,m;
+    while(l<=h){
+        m=Math.floor((l+h)/2)
+        if(a[m]==i){
+            return m 
+        }
+        else if(a[m]>i){
+            h=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+}
+console.log("Binary search");
+console.log(binary_search(a, 1)); //0
+console.log(binary_search(a, 5)); //4
+//12
+a=[1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6];
+m=0;
+ans=new Set();
+for(i=0;i<a.length;i++){
+    m=0;
+    for(j=0;j<a.length;j++){
+        if(a[i]===a[j]){
+            m+=1;
+        }
+    }
+    if(m>1){
+        ans.add(a[i]);
+    }
+}
+console.log(ans);
